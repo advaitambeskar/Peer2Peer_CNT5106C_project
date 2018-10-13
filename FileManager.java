d7edd6ca5845c80c4ac872860a8f9e5e42877f5b
Gao, Xiang <qasdfgtyuiop@gmail.com>
2018-10-13 14:15:23 -0400

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -6,6 +6,9 @@ public class FileManager {
     public byte [] getPiece(int index) {
         return new byte[]{};
     }
+    public int getPieceLength(int index) {
+        return 100;
+    }
     public void setPiece(int index, byte [] payload) {
         return;
     }
